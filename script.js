cb29b383c9b13978d8ebb315ca436abe4da72112
Utkarsh Kumar Patel <88320546+Utkarsh7545@users.noreply.github.com>
2023-03-19 19:25:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your code here
+function majorityelement(arr) {
+	let a=floor(arr.length/2);
+	for (let i = 0; i < arr.length; i++) {
+		let count=1;
+		for (let j = i+1; j < arr.length; j++) {
+			if(arr[i]==arr[j]){
+				count++;
+			}
+		}
+		if(count>=a){
+			return arr[i];
+		}
+	}
+	return 0;
+}
+const readline = require("readline");
+
+const input = [];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (cmd) {
+  input.push(cmd);
+});
+
+rl.on("close", function (cmd) {
+  Main(input);
+});
+
+function Main(input) {
+  const arr = JSON.parse(input[0]).map((e) => parseInt(e, 10));
+  console.log(majorityelement(arr));
+}
